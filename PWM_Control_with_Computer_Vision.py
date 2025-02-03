bf6b2ac1bea0f7a236d0dcb626b50d4c1125a798
William Souza <souzawpessoal2020@gmail.com>
2025-02-03 20:21:35 -0300

    setting medidapipe library parameters

--- PWM_Control_with_Computer_Vision.py
+++ PWM_Control_with_Computer_Vision.py
@@ -2,10 +2,14 @@
 #                                                           #
 #           PWM CONTROL WITH COMPUTER VISION                #
 #                                                           #
-#                                                           #
+#                      William Santos                       #
 
 
 import cv2
 import mediapipe as mp
 
 video = cv2.VideoCapture(0) # find the camera of the PC
+
+hand = mp.solutions.hands # from library mediapipe. In the case we use hand map
+Hands = hand.Hands(max_num_hands=1) # Max number of hands that sw recognize. # Hand detection
+mpDraw = mp.solutions.drawing_utils # Responsible to draw the ligation on the hand. 
