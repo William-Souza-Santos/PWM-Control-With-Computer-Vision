017a9675af91dc99143dd0963e957b06dde28450
William Souza <souzawpessoal2020@gmail.com>
2025-02-03 21:52:12 -0300

    left hand logic is done

--- PWM_Control_with_Computer_Vision.py
+++ PWM_Control_with_Computer_Vision.py
@@ -20,14 +20,31 @@ while True:
     results = Hands.process(imgRGB)
     handsPoints = results.multi_hand_landmarks # obtain the condinators of the hand
     h, w, _ = img.shape  # obtain dimension of image h higher w width
+    pontos = []
     if handsPoints:
         for points in handsPoints:
             #(points) # is visible see the the lendmarks
             mpDraw.draw_landmarks(img, points, hand.HAND_CONNECTIONS)
             for id, cord in enumerate(points.landmark):
                 cx, cy = int(cord.x * w), int(cord.y * h) # variable conveter in pixel
-                cv2.putText(img,str(id),(cx,cy+10),cv2.FONT_HERSHEY_SIMPLEX,0.5,(255,0),2) # IS POSSIBLE TO SEE EACH NUMBER IN THE HANDLE
+                #cv2.putText(img,str(id),(cx,cy+10),cv2.FONT_HERSHEY_SIMPLEX,0.5,(255,0),2) # IS POSSIBLE TO SEE EACH NUMBER IN THE HANDLE #Debughand
+                pontos.append((cx, cy))
+                #print(pontos)
 
+    dedos = [8, 12, 16, 20]  #this values are reference  in mediapipe library (left hand)
+
+# left hand logic
+
+    contador = 0
+    if pontos:
+        if pontos[4][0] < pontos[2][0] :  # lógica do dedão
+            contador = +1
+        for x in dedos:
+            if pontos[x][1] < pontos[x - 2][1]:  # [posição : 8,12,16,20] [valor] #4 dedos mão esquerda
+                contador += 1
+
+
+    print(contador) # is possible to know how many fingers are lift
 
     cv2.imshow("Imagem",img)
     cv2.waitKey(1)
